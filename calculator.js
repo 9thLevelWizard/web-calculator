5e4f0e3a799a13c1d4df401046da6e7f5f9b6d3e
9thLevelWizard <154519142+9thLevelWizard@users.noreply.github.com>
2024-02-24 12:03:13 +0000

    Feat: Simple arithmetic functions
    Added the basic arithmetic functions, plus a separate operate() function.

---
+++ calculator.js
@@ -0,0 +1,38 @@
+let x, y;
+let operator;
+
+function add(x, y) {
+    return x + y;
+};
+
+function subtract(x, y) {
+    return x - y;
+};
+
+function multiply(x, y) {
+    return x * y;
+};
+
+function divide(x, y) {
+    return x / y;
+};
+
+function operate(operator, x, y) {
+    switch (operator) {
+        case "+":
+            add(x, y);
+            break;
+        case "-":
+            subtract(x, y);
+            break;
+        case "*":
+            multiply(x, y);
+            break;
+        case "/":
+            divide(x, y);
+            break;
+        default:
+            console.log("Invalid operator.");
+            break;
+    };
+};
